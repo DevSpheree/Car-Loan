a2acde07a5556c7a2e129057502988ea1953ea18
Migueaali12 <malvarez11@utpl.edu.ec>
2025-01-14 12:27:51 -0500

    frotend config

--- App/car-loan-go/routes/vehicleRoutes.go
+++ App/car-loan-go/routes/vehicleRoutes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"car-loan-go/handlers"
+	"car-loan-go/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupVehicleRoutes(app *fiber.App) {
     // Create a vehicle group with authentication middleware
-    vehicle := app.Group("/vehicles")
+    vehicle := app.Group("/vehicles", middlewares.AuthMiddleware())
     
     // All routes in this group will require authentication
     vehicle.Get("/", handlers.GetAllVehicles)
--- App/car-loan-react/app/(tabs)/index.tsx
+++ App/car-loan-react/app/(tabs)/index.tsx
@@ -1,155 +1,202 @@
-import React, { useState } from "react";
-import {
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    StyleSheet,
-    Image,
-} from "react-native";
+import React from 'react'
+import * as yup from 'yup'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+type LoginFormType = {
+  email: string
+  password: string
+}
+
+const loginSchema = yup.object({
+  email: yup.string().required('El email es requerido').email('Ingrese un email válido'),
+  password: yup
+    .string()
+    .required('La contraseña es requerida')
+    .min(6, 'La contraseña debe tener al menos 6 caracteres')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+      'La contraseña debe contener al menos una mayúscula, una minúscula y un número'
+    ),
+})
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormType>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: yupResolver(loginSchema),
+  })
 
-    const handleLogin = () => {
-        // Aquí puedes manejar la lógica de autenticación
-        console.log("Email:", email, "Password:", password);
-    };
+  const onSubmit = (data: LoginFormType) => {
+    console.log('Form data:', data)
+  }
 
-    return (
-        <View style={styles.container}>
-            {/* Encabezado con imagen y texto */}
-            <View style={styles.header}>
-                <Text style={styles.welcomeText}>Bienvenido a</Text>
-                <Image
-                    source={require("../../assets/images/checklist1.png")} // Cambia por la ruta de tu logo
-                    style={styles.logo1}
-                    resizeMode="contain"
-                />
-                <Image
-                    source={require("../../assets/images/logo.png")} // Cambia por la ruta de tu logo
-                    style={styles.logo}
-                    resizeMode="contain"
-                />
-            </View>
+  return (
+    <View style={styles.container}>
+      {/* Encabezado con imagen y texto */}
+      <View style={styles.header}>
+        <Text style={styles.welcomeText}>Bienvenido a</Text>
+        <Image
+          source={require('@/assets/images/checklist1.png')}
+          style={styles.logo1}
+          resizeMode="contain"
+        />
+        <Image
+          source={require('@/assets/images/logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
 
-            {/* Contenido de inicio de sesión */}
-            <View style={styles.loginContainer}>
-                <Text style={styles.loginTitle}>Inicio de sesión</Text>
+      {/* Contenido de inicio de sesión con React Hook Form */}
+      <View style={styles.formContainer}>
+        <Controller
+          control={control}
+          rules={{
+            required: 'El email es requerido',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'Email inválido',
+            },
+          }}
+          name="email"
+          render={({ field: { onChange, value } }) => (
+            <>
+              <TextInput
+                style={styles.input}
+                placeholder="Email"
+                onChangeText={onChange}
+                value={value}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+              {errors.email && <Text style={styles.errorText}>{errors.email.message}</Text>}
+            </>
+          )}
+        />
 
-                {/* Input de correo */}
-                <TextInput
-                    style={styles.input}
-                    placeholder="Correo Institucional"
-                    placeholderTextColor="#000"
-                    value={email}
-                    onChangeText={setEmail}
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                />
+        <Controller
+          control={control}
+          rules={{
+            required: 'La contraseña es requerida',
+            minLength: {
+              value: 6,
+              message: 'La contraseña debe tener al menos 6 caracteres',
+            },
+          }}
+          name="password"
+          render={({ field: { onChange, value } }) => (
+            <>
+              <TextInput
+                style={styles.input}
+                placeholder="Contraseña"
+                onChangeText={onChange}
+                value={value}
+                secureTextEntry
+              />
+              {errors.password && <Text style={styles.errorText}>{errors.password.message}</Text>}
+            </>
+          )}
+        />
 
-                {/* Input de contraseña */}
-                <TextInput
-                    style={styles.input}
-                    placeholder="Contraseña"
-                    placeholderTextColor="#000"
-                    value={password}
-                    onChangeText={setPassword}
-                    secureTextEntry
-                />
+        <TouchableOpacity style={styles.loginButton} onPress={handleSubmit(onSubmit)}>
+          <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
 
-                <View style={styles.inputContainer}>
-                    {/* Botón para recuperar contraseña */}
-                    <TouchableOpacity>
-                        <Text style={styles.forgotPassword}>Recupera tu contraseña</Text>
-                    </TouchableOpacity>
-                </View>
-
-
-
-                {/* Botón de inicio de sesión */}
-                <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-                    <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
-};
-
+// Add this to your existing styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FFF",
-    },
-    header: {
-        backgroundColor: "#004270",
-        alignItems: "center",
-        justifyContent: "center",
-        paddingTop: 80,
-        paddingHorizontal: 10,
-        borderBottomRightRadius: 20,
-        borderBottomLeftRadius: 20,
-    },
-    welcomeText: {
-        color: "#FFF",
-        fontSize: 16,
-        marginBottom: 10,
-        fontWeight: "400",
-    },
-    logo1: {
-        width: 156,
-        height: 104,
-    },
-    logo: {
-        width: 180,
-        height: 120,
-    },
-    loginContainer: {
-        flex: 1,
-        padding: 20,
-        alignItems: "center",
-        marginTop: 30,
-    },
-    loginTitle: {
-        fontSize: 24,
-        fontWeight: "bold",
-        color: "#004270",
-        marginBottom: 30,
-    },
-    inputContainer: {
-        flexDirection: "row", // Orientación horizontal
-        justifyContent: "flex-end",
-        width: "100%",
-        marginBottom: 15,
-    },
-    input: {
-        width: "100%",
-        height: 50,
-        backgroundColor: "transparent",
-        paddingHorizontal: 15,
-        marginBottom: 15,
-        borderBottomWidth: 1,
-        borderColor: "#E9B40A",
-    },
-    forgotPassword: {
-        color: "#034872",
-        textDecorationLine: "underline",
-        marginBottom: 20,
-    },
-    loginButton: {
-        backgroundColor: "#004270",
-        paddingVertical: 15,
-        paddingHorizontal: 20,
-        borderRadius: 8,
-        width: "100%",
-        alignItems: "center",
-    },
-    loginButtonText: {
-        color: "#FFF",
-        fontSize: 16,
-        fontWeight: "bold",
-    },
-});
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  header: {
+    backgroundColor: '#004270',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 80,
+    paddingHorizontal: 10,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
+  welcomeText: {
+    color: '#FFF',
+    fontSize: 16,
+    marginBottom: 10,
+    fontWeight: '400',
+  },
+  logo1: {
+    width: 156,
+    height: 104,
+  },
+  logo: {
+    width: 180,
+    height: 120,
+  },
+  loginContainer: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  loginTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#004270',
+    marginBottom: 30,
+  },
+  inputContainer: {
+    flexDirection: 'row', // Orientación horizontal
+    justifyContent: 'flex-end',
+    width: '100%',
+    marginBottom: 15,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    backgroundColor: 'transparent',
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderColor: '#E9B40A',
+  },
+  forgotPassword: {
+    color: '#034872',
+    textDecorationLine: 'underline',
+    marginBottom: 20,
+  },
+  loginButton: {
+    backgroundColor: '#004270',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    width: '100%',
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 12,
+    marginBottom: 10,
+  },
+  formContainer: {
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+})
 
-export default LoginScreen;
+export default LoginScreen
