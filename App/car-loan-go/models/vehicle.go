11e81ea791357287ba7b07a7d73ce01074e839c7
Migueaali12 <malvarez11@utpl.edu.ec>
2025-01-19 18:30:14 -0500

    vehicle rest api updated

--- App/car-loan-go/models/vehicle.go
+++ App/car-loan-go/models/vehicle.go
@@ -1,15 +1,21 @@
 package models
 
 type Vehicle struct {
-    ID           string  `json:"id" firestore:"-"`
-    Type         string  `json:"type" firestore:"type" validate:"required,oneof=light heavy"`
-    Brand        string  `json:"brand" firestore:"brand" validate:"required,min=2,max=50"`
-    Color        string  `json:"color" firestore:"color" validate:"required,min=2,max=30"`
-    Model        string  `json:"model" firestore:"model" validate:"required,min=1,max=50"`
-    Weight       int     `json:"weight" firestore:"weight" validate:"required,min=1"`
-    Year         int     `json:"year" firestore:"year" validate:"required,min=1900,max=2025"`
-    FuelCapacity int     `json:"fuel_capacity" firestore:"fuel_capacity" validate:"required"`
-    FuelType     string  `json:"fuel_type" firestore:"fuel_type" validate:"required,oneof=gasoline diesel electric hybrid"`
-    KmActual     float32 `json:"km_actual" firestore:"km_actual" validate:"required,min=0"`
-    KmGallon     float32 `json:"km_gallon" firestore:"km_gallon" validate:"required,min=0"`
+	ID               string  `json:"id" firestore:"-"`
+	ActivityLocation string  `json:"activity_location" firestore:"activity_location" validate:"required,min=2,max=50"`
+	BrandYear        string  `json:"brand_year" firestore:"brand_year" validate:"required,min=2,max=100"`
+	Chasis           string  `json:"chasis" firestore:"chasis" validate:"required,min=2,max=100"`
+	Engine           string  `json:"engine" firestore:"engine" validate:"required,min=2,max=100"`
+	Fuel             string  `json:"fuel" firestore:"fuel" validate:"required,oneof=GASOLINA ELECTRICO DIESEL HIBRIDO NO_APLICA"`
+	Num              int     `json:"num" firestore:"num" validate:"required"`
+	Property         string  `json:"property" firestore:"property" validate:"required,min=2,max=50"`
+	Responsible      string  `json:"responsible" firestore:"responsible" validate:"required,min=2,max=50"`
+	Type             string  `json:"type" firestore:"type" validate:"required,min=2,max=25"`
+	VehiclePlate     string  `json:"vehicle_plate" firestore:"vehicle_plate" validate:"required,min=2,max=10"`
+	Brand            string  `json:"brand" firestore:"brand" validate:"required,min=2,max=50"`
+	Color            string  `json:"color" firestore:"color" validate:"required,min=1,max=30"`
+	Year             int     `json:"year" firestore:"year" validate:"required,min=1900,max=2025"`
+	FuelCapacity     int     `json:"fuel_capacity" firestore:"fuel_capacity" validate:"min=0"`
+    KmActual         float32 `json:"km_actual" firestore:"km_actual" validate:"min=0"`
+    KmGallon         float32 `json:"km_gallon" firestore:"km_gallon" validate:"min=0"`
 }
